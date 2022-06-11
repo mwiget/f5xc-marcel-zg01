ad11e6f689d00d1543b7952ace2cfb839fc77dff
Marcel Wiget <mwiget@gmail.com>
2022-06-11 12:33:37 +0000

    display url

--- set-env.sh
+++ set-env.sh
@@ -4,6 +4,8 @@ export VOLT_API_P12_FILE=$PWD/.api-creds.p12
 if [ ! -z "$VOLT_API_CA_CERT" ]; then
   echo $VOLT_API_CA_CERT | base64 -d > $PWD/.public_server_ca.crt
   export VOLT_API_CA_CERT=$PWD/.public_server_ca.crt
+  echo VOLT_API_CA_CERT=$VOLT_API_CA_CERT
 fi
 echo VOLT_API_P12_FILE=$VOLT_API_P12_FILE
-grep set-env.sh ~/.bashrc || echo "source $PWD/set-env.sh" >> ~/.bashrc
+echo VOLT_API_URL=$VOLT_API_URL
+grep set-env.sh ~/.bashrc >/dev/null || echo "source $PWD/set-env.sh" >> ~/.bashrc
