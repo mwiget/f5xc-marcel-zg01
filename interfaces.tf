2f7cc78e164cff7210611646b6dd60671013cccb
Marcel Wiget <mwiget@gmail.com>
2022-06-11 19:47:54 +0000

    renumber network

--- interfaces.tf
+++ interfaces.tf
@@ -6,15 +6,15 @@ resource "volterra_network_interface" "eth1" {
     device = "eth1"
     dhcp_server {
       dhcp_networks {
-        network_prefix = "192.168.2.0/24"
+        network_prefix = "192.168.40.0/24"
         pool_settings = "INCLUDE_IP_ADDRESSES_FROM_DHCP_POOLS"
         first_address = true
         same_as_dgw = true
       }
       fixed_ip_map = {
-        "3c:ec:ef:45:3b:30" = "192.168.2.100"
-        "3c:ec:ef:45:3b:64" = "192.168.2.161"
-        "b8:ce:f6:44:d1:ea" = "192.168.2.199"
+        "3c:ec:ef:45:3b:30" = "192.168.40.100"
+        "3c:ec:ef:45:3b:64" = "192.168.40.161"
+        "b8:ce:f6:44:d1:ea" = "192.168.40.199"
       }
       automatic_from_start = true
     }
